b2b72137b5dc089c3f1f010591e925f00bce89b9
David Robert <130157683+devdavidrobert@users.noreply.github.com>
2025-02-11 21:53:10 +0300

    Delete log/ss_credentials.py

--- log/ss_credentials.py
+++
@@ -1,7 +0,0 @@
-####################################################################################
-class ss_log:
-    def __init__(self, username = "d.robert@bboxx.co.uk", pswd = "Bl3$$3d0n3"):
-        self.username = username
-        self.pswd = pswd
-    
-default_credential = ss_log()
